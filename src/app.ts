0bfe6be130fc3044c58ba288f54557bdb1ce3add
Sangchan <sangchan82@gmail.com>
2023-03-08 17:59:41 +0900

    put comment from original cases to migrate to swagger

--- src/app.ts
+++ src/app.ts
@@ -15,39 +15,23 @@ app.get('/', (req, res, next) => {
     res.send('Hello')
 })
 
-/* GET sign in page. */
 /**
-* @swagger
-* paths:
-*  /signin:
-*   get:
-*     tags: [SignIn]
-*     summary: 로그인 페이지
-*     responses:
-*       "200":
-*         description: 로그인 페이지 로드 성공
-*   post:
-*     tags: [SignIn]
-*     summary: 로그인 로직 처리
-*     parameters:
-*       - name: code
-*         in: Post
-*         type: string
-*         description: 로그인 정보(아이디)
-*     responses:
-*       "200":
-*         discription: 로그인 성공
-*         contnet:
-*           application:json
-*       "400":
-*         discription: 잘못된 파라메타 전달
-*     
-*/
-
+ * @path {GET} http://localhost:12345/api/users
+ * @description 요청 데이터 값이 없고 반환 값이 있는 GET Method
+ */
 app.get("/api/users", (req, res) => {
     res.json({ok: true, users: users});
 })
 
+/**
+ * @path {GET} http://localhost:12345/api/users/user?user_id=1
+ * @description Query Params 요청 데이터 값이 있고 반환 값이 있는 GET Method 
+ * 
+ *  Query Params 방식
+ *  user 뒤에 user_id변수를 통해 값을 찾아 올수 있다.
+ *  &를 통해 두번째 변수를 받아서 사용할 수 있다.(/user?user_id=1&name="유저1")
+ * 
+ */
 app.get("/api/users/user", (req, res) => {
 
     const user_id = parseInt(req.query.user_id as string)
@@ -56,6 +40,12 @@ app.get("/api/users/user", (req, res) => {
     res.json({ok: false, user: user})
 })
 
+/**
+ * @path {GET} http://localhost:12345/api/users/userBody
+ * @description Body 요청 데이터 값이 있고 반환 값이 있는 GET Method 
+ * 
+ *  post로 요청시 body에 데이터를 담아서 보낼수 있듯이 get도 사용이 가능하다.
+ */
 app.get("/api/users/userBody", (req, res) => {
 
     const user_id = parseInt(req.query.user_id as string)
@@ -64,6 +54,20 @@ app.get("/api/users/userBody", (req, res) => {
     res.json({ok: false, user: user})
 })
 
+/**
+ * @path {GET} http://localhost:12345/api/users/:user_id
+ * @description Path Variables 요청 데이터 값이 있고 반환 값이 있는 GET Method 
+ * 
+ *  Path Variables 방식
+ * 
+ *  ex) 아래 GET 주소 에서 :user_id 는 서버에서 설정한 주소 키 값이다.
+ *      값을 찾을 때는 req.params.user_id 로 값을 찾는다.
+ * 
+ *  *주의 사항*
+ *  :user_id 이 부분은 변수이기 때문에 
+ *  경로가 /users/1 이거나 /users/2 이거 일때 둘다 라우터를 거치게 된다.
+ *  그렇기 때문에 다른 라우터 보다 아래 있어야 한다.
+ */
 app.get("/api/users/:user_id", (req, res) => {
 
     const user_id = parseInt(req.query.user_id as string)
@@ -72,6 +76,13 @@ app.get("/api/users/:user_id", (req, res) => {
     res.json({ok: true, user: user})
 })
 
+/**
+ * @path {POST} http://localhost:12345/api/users/add
+ * @description POST Method
+ * 
+ *  POST 데이터를 생성할 때 사용된다.
+ *  req.body에 데이터를 담아서 보통 보낸다.
+ */
 app.post("/api/users/add", (req, res) => {
 
     const { id, name } = req.body
@@ -80,6 +91,10 @@ app.post("/api/users/add", (req, res) => {
     res.json({ok: true, users: user})
 })
 
+/**
+ * @path {PUT} http://localhost:12345/api/users/update
+ * @description 전체 데이터를 수정할 때 사용되는 Method
+ */
 app.put("/api/users/update", (req, res) => {
     
     const { id, name } = req.body
@@ -98,6 +113,10 @@ app.put("/api/users/update", (req, res) => {
     res.json({ok: true, users: user})
 })
 
+/**
+ * @path {PATCH} http://localhost:12345/api/user/update/:user_id
+ * @description 단일 데이터를 수정할 때 사용되는 Method
+ */
 app.patch("/api/user/update/:user_id", (req, res) => {
 
     const { user_id } = req.params
@@ -117,6 +136,11 @@ app.patch("/api/user/update/:user_id", (req, res) => {
     res.json({ok: true, users: user})
 })
 
+/**
+ * @path {DELETE} http://localhost:12345/api/user/delete
+ * @description 데이터 삭제
+ * 
+ */
 app.delete("/api/user/delete", (req, res) => {
 
     const user_id = parseInt(req.query.user_id as string)
