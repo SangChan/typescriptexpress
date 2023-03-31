701b613b2bffe4e4f8d1c37af042fa799cea4d53
Sangchan <sangchan82@gmail.com>
2023-03-31 18:11:12 +0900

    add new

--- src/routes/api/parse.ts
+++ src/routes/api/parse.ts
@@ -1,40 +1,36 @@
 import express, { Request, Response, NextFunction } from 'express'
 const parse = {
     link : function(req : Request, res: Response) {
-        interface ErrorCheckType {
+        interface ErrorCheckIntrerface {
             result : String
             message : String
             errorMessage() : String
         }
+
+        class ErrorCheckType implements ErrorCheckIntrerface {
+            result : String
+            message : String
+
+            constructor(result: Boolean) {
+                this.result = result ? `success` : `fail`
+                this.message = this.errorMessage()
+            }
+
+            errorMessage() {
+                return "valid"
+            }
+        }
+
+
         const url = new URL(req.query.url as string)
         console.log("protocol: ",url.protocol)
         console.log("hostname: ",url.hostname)
         console.log("path:",url.pathname)
         console.log("query:",url.search)
         console.log("query:",url.searchParams.keys())
-        const scheme_check : ErrorCheckType = {
-            result : success((url.protocol === "https:") || url.toString().startsWith('https://')),
-            message : "valid",
-            errorMessage() {
-                "valid"
-            }
-        }
-
-        const path_check : ErrorCheckType = {
-            result : success(url.pathname.length > 1),
-            message : "valid",
-            errorMessage() {
-                "valid"
-            }
-        }
-
-        const query_check : ErrorCheckType = {
-            result : success(true),
-            message : "valid",
-            errorMessage() {
-                "valid"
-            }
-        }
+        const scheme_check : ErrorCheckType = new ErrorCheckType((url.protocol === "https:") || url.toString().startsWith('https://'))
+        const path_check : ErrorCheckType = new ErrorCheckType(url.pathname.length > 1)
+        const query_check : ErrorCheckType = new ErrorCheckType(true)
 
         res.json({
             url: url.toString(),
@@ -44,7 +40,5 @@ const parse = {
         })  
     }
 }
-
-const success = (value: Boolean) =>  value ? `success` : `fail`
 
 module.exports = parse
