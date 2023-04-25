4982d3a0cff8c99a067dc98922749029d5bb7ed9
Sangchan <sangchan82@gmail.com>
2023-04-25 17:48:07 +0900

    add new

--- src/routes/api/parse.ts
+++ src/routes/api/parse.ts
@@ -23,6 +23,23 @@ class SchemChecker implements ErrorCheckIntrerface {
     
 }
 
+class HostChecker implements ErrorCheckIntrerface {
+    result: String
+    message: String
+
+    constructor(host: String) {
+        console.log("host : ", host)
+        const res :boolean = host.length > 0
+        this.result = res ? 'success' : 'fail'
+        this.message = this.errorMessage(res)
+    }
+
+    errorMessage(res: Boolean): String {
+        return res ? "valid" : "not valid"
+    }
+    
+}
+
 class PathChecker implements ErrorCheckIntrerface {
     result: String
     message: String
@@ -63,16 +80,19 @@ class QueryChecker implements ErrorCheckIntrerface {
 const parse = {
     link : function(req : Request, res: Response) {
         console.log("scheme = ", req.query.scheme)
+        console.log("scheme = ", req.query.host)
         console.log("path = ", req.query.path)
         console.log("query = ", req.query.query)
         const url = new URL(req.query.url as string)
         const scheme_check : ErrorCheckIntrerface = new SchemChecker(url.protocol)
+        const host_check : ErrorCheckIntrerface = new HostChecker(url.host)
         const path_check : ErrorCheckIntrerface = new PathChecker(url.pathname)
         const query_check : ErrorCheckIntrerface = new QueryChecker(url.searchParams)
 
         res.json({
             url: url.toString(),
             schemeCheck: scheme_check,
+            hostCheck : host_check,
             pathCheck: path_check,
             queryCheck: query_check
         })  
