ca0da008d4865352d688aed52b1e50492069d486
Sangchan <sangchan82@gmail.com>
2023-04-27 16:49:13 +0900

    add new

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -11,4 +11,21 @@ router.get("/parseLink", (req, res) => {
     parse.link(req, res)
 })
 
+
+router.get('/example', (req, res) => {
+    const urlString = req.query.url as string; // assuming the URL parameter is passed as a query parameter
+    const parsedUrl = new URL(urlString); // parse the URL using the built-in URL constructor
+    const scheme = parsedUrl.protocol; // get the scheme (http or https)
+    const host = parsedUrl.host; // get the host (including the port if specified)
+    const path = parsedUrl.pathname; // get the path (including the leading slash)
+    const query = parsedUrl.search; // get the query string (including the leading question mark)
+  
+    console.log('Scheme:', scheme);
+    console.log('Host:', host);
+    console.log('Path:', path);
+    console.log('Query:', query);
+  
+    res.send('Example GET request');
+  });
+
 module.exports = router
