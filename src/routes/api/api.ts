bbf31a72393120f829fb13ff8d5f0a5c6bf80f3b
Sangchan <sangchan82@gmail.com>
2023-05-03 18:16:35 +0900

    add new

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -2,23 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 const parse = require('./parse')
+const test = require('./test')
 
 router.get('/', (req, res, next) => {
-    res.send('[route] api index')
-    const url = require('url');
-
-    const urlString = 'https://www.example.com/path/to/resource?key1=value1&key2=value2#section1';
-
-    const parsedUrl = new URL(urlString);
-
-    const protocol = parsedUrl.protocol; // "https:"
-    const host = parsedUrl.host; // "www.example.com"
-    const hostname = parsedUrl.hostname; // "www.example.com"
-    const port = parsedUrl.port; // "" (if not specified in the URL)
-    const path = parsedUrl.pathname; // "/path/to/resource"
-    const search = parsedUrl.search; // "?key1=value1&key2=value2"
-    const hash = parsedUrl.hash; // "#section1"
-    console.log("search:", search)
+    test.root(req, res)
 })
 
 router.get("/parseLink", (req, res) => {
@@ -27,20 +14,7 @@ router.get("/parseLink", (req, res) => {
 
 
 router.get('/example', (req, res) => {
-    const url = require('url');
-    const urlString = req.query.url as string; // assuming the URL parameter is passed as a query parameter
-    const parsedUrl = new URL(urlString); // parse the URL using the built-in URL constructor
-    const scheme = parsedUrl.protocol; // get the scheme (http or https)
-    const host = parsedUrl.host; // get the host (including the port if specified)
-    const path = parsedUrl.pathname; // get the path (including the leading slash)
-    const query = parsedUrl.search; // get the query string (including the leading question mark)
-  
-    console.log('Scheme:', scheme);
-    console.log('Host:', host);
-    console.log('Path:', path);
-    console.log('Query:', query);
-  
-    res.send('Example GET request');
-  });
+    test.example(req, res)
+});
 
 module.exports = router
---
+++ src/routes/api/test.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response, NextFunction } from 'express'
+const test = {
+    root : function(req : Request, res: Response) {
+        const url = require('url');
+
+        const urlString = 'https://www.example.com/path/to/resource?key1=value1&key2=value2#section1';
+
+        const parsedUrl = new URL(urlString);
+
+        const protocol = parsedUrl.protocol; // "https:"
+        const host = parsedUrl.host; // "www.example.com"
+        const hostname = parsedUrl.hostname; // "www.example.com"
+        const port = parsedUrl.port; // "" (if not specified in the URL)
+        const path = parsedUrl.pathname; // "/path/to/resource"
+        const search = parsedUrl.search; // "?key1=value1&key2=value2"
+        const hash = parsedUrl.hash; // "#section1"
+        console.log("search:", search)
+        res.send('[route] api index')
+    },
+    example : function(req : Request, res: Response) { 
+        const url = require('url');
+        const urlString = req.query.url as string; // assuming the URL parameter is passed as a query parameter
+        const parsedUrl = new URL(urlString); // parse the URL using the built-in URL constructor
+        const scheme = parsedUrl.protocol; // get the scheme (http or https)
+        const host = parsedUrl.host; // get the host (including the port if specified)
+        const path = parsedUrl.pathname; // get the path (including the leading slash)
+        const query = parsedUrl.search; // get the query string (including the leading question mark)
+    
+        console.log('Scheme:', scheme);
+        console.log('Host:', host);
+        console.log('Path:', path);
+        console.log('Query:', query);
+    
+        res.send('Example GET request');
+    }
+}
+
+module.exports = test
