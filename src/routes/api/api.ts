d9417d30d2b27575a156265836bf4580342e2200
Sangchan <sangchan82@gmail.com>
2023-06-09 19:28:04 +0900

    add new

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 const router = express.Router()
 const parse = require('./parse')
-const parse2 = require('./parse2')
+const parse2 = require('./parseExperiments')
 
 router.post('/example', (req, res) => {
     parse2.example(req, res)
