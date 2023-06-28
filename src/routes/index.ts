3a175dcedf00b8ee2ad6a31a514a56fb610f4561
Sangchan <sangchan82@gmail.com>
2023-06-28 21:46:02 +0900

    missing files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,8 @@ import express from 'express'
 const router = express.Router()
 
 router.get('', (req, res) => {
-    res.send('<h1> I am Iron Man</h1>')
+    //res.send('<h1> I am Iron Man</h1>')
+    res.sendFile('./views/index.html')
 })
 
 router.get('/about',(req, res) => {
