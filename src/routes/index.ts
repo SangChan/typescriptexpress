b65e71f887a9868de64524e221003ef3e853085c
Sangchan <sangchan82@gmail.com>
2023-06-30 15:50:29 +0900

    add new

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,14 @@ import express from 'express'
 
 const path = require('path')
 const router = express.Router()
+const __viewsName = path.join(__dirname, '..', 'views')
 
 router.get('', (req, res) => {
-    //res.send('<h1> I am Iron Man</h1>')
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+    res.sendFile(path.join(__viewsName, 'index.html'))
 })
 
 router.get('/about',(req, res) => {
-    res.send('<h1>This is about page</h1>')
+    res.sendFile(path.join(__viewsName, 'about.html'))
 })
 
 module.exports = router
