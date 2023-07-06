abf200f4151f26b2214b480bfef92b3a759389c0
Sangchan <sangchan82@gmail.com>
2023-07-06 19:25:39 +0900

    add new

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import path from 'path';
 const __dirname = path.resolve();
 
-const indexRouter = express()
+export const indexRouter = express()
 const __viewsName = path.join(__dirname, '..', 'views')
 indexRouter.set('views', __viewsName)
 indexRouter.set('view engine', 'pug')
@@ -18,5 +18,3 @@ indexRouter.get('/about',(req, res) => {
 indexRouter.get('/pug', (req, res) => {
     res.render('template.pug', {title: 'PUG : EXPRESS TEMPLATE ENGINE'})
 })
-
-module.exports = exports = indexRouter
