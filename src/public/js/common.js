a1f2cd8016ee72f01c4714db4b5bfc92abffc709
Sangchan <sangchan82@gmail.com>
2023-07-27 13:38:09 +0900

    add new

--- src/public/js/common.js
+++ src/public/js/common.js
@@ -1,1 +1,10 @@
 export var a = "A"
+
+module.exports = function(req, res, next){
+    req.database.query("SELECT * FROM category;", (err, categories) => {
+        if (err) throw err;
+
+        req.categories = categories;
+        next();
+    })
+}
